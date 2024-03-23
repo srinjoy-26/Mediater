fab44eb0c5faffd109ef1d1d2c2cae40698bfcab
srinjoy-26 <srinjoydev26@gmail.com>
2024-03-23 07:42:02 +0530

    created Navbar , implemented routes , added assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import './App.css'
-
+import { Route , Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/About'
 export default function App() {
   return (
-   <></>
+   <>
+     <div className='bg-gradient-to-b from-pink-700 to bg-purple-700  md:h-screen'>
+     <Navbar/>
+     <Routes>
+         <Route path='/' element={<Home/>}></Route>
+         <Route path='/about' element={<About/>}></Route>
+         <Route path='/contact' element={<Contact/>}></Route>
+     </Routes>
+     <Footer/>
+     </div>
+   </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
 
 
 const Navbar = () => {
+  let Navitems = ['Home' , 'About' , 'Contact']
   return (
-    <div>
-      
+  <div className=" flex justify-between items-center bg-slate-200  px-7  ">
+       <div className="logo h-20 w-20"></div> 
+     
+      <div id="Nvlinks" className="flex gap-8">
+        {Navitems.map((x,index)=>{
+          return(
+            <p key={index} className="text-lg ">{x}</p>
+          )
+        })}
+      </div>
     </div>
   )
 }
