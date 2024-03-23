93e53edf1da06e9fc14ffb986be236ccb727d823
srinjoy-26 <srinjoydev26@gmail.com>
2024-03-23 22:45:11 +0530

    added animations , aos library

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   let Navitems = ['Home' , 'About' , 'Contact']
   return (
   <div className=" flex justify-between items-center  px-8  ">
-       <motion.div className="logo h-[4.5rem] w-20 bg-white rounded-md" ></motion.div> 
+       <motion.div className="logo h-[4.5rem] w-[4.5rem] bg-white rounded-md" animate={{rotate:[0,100,100,0] , x:[0,100,100,0]}} transition={{duration: 1}}></motion.div> 
      
       <div id="Nvlinks" className="flex gap-8">
         {Navitems.map((x,index)=>{
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,21 @@
-
+import Aos from "aos";
+import 'aos/dist/aos.css'
+import { useEffect } from "react";
 
 const Home = () => {
+  useEffect(()=>{
+    Aos.init({duration: 1000})
+  },[])
   return (
     <div className="flex flex-col justify-center items-center gap-3 md:gap-5 md:h-[70vh] lg:flex-row " id="home-div">
-      <div className="lg:w-[40%] md:w-[70%] flex flex-col gap-4 py-5 px-3 lg:px-0 " >
+      <div className="lg:w-[40%] md:w-[70%] flex flex-col gap-4 py-5 px-3 lg:px-0 " data-aos = "fade-right" >
         <h1 className="text-4xl md:text-5xl text-white" id="home-heading">Connecting Brands with Creativity</h1>
         <p className=" text-xl md:text-2xl text-white">Welcome to Mediater - Your Gateway to Digital Collaboration!</p>
         <p className="text-lg md:text-xl text-slate-200 tracking-wide">At Mediater , we redefine collaboration by seamlessly connecting content creators with businesses, fostering a dynamic ecosystem of innovation and creativity. Our platform serves as the catalyst for strategic alliances, unlocking endless possibilities in the digital landscape. Join the revolution, where every collaboration is an opportunity to elevate and innovate.</p>
         
       </div>
 
-      <div className="flex flex-col gap-5 py-5 px-3 lg:px-0 ">
+      <div className="flex flex-col gap-5 py-5 px-3 lg:px-0 "  data-aos = "fade-left">
       <p className="text-lg md:text-xl text-white">Elevate Your Experience: Subscribe Now for Exclusive Early Access!</p>
       <form className="flex flex-col gap-4">
         <input type="text" className="focus:outline-none rounded-lg p-3 md:w-[80%] w-full mx-auto" placeholder="Email Address"/>
