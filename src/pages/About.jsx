9d6034ef5de81fe29288f3f2a87f6e7ef5f598da
srinjoy-26 <srinjoydev26@gmail.com>
2024-03-27 20:37:32 +0530

    contact page complete

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import vision from '../assets/shared-vision.png'
 import Aos from "aos";
 import 'aos/dist/aos.css'
 import { useEffect } from "react";
-
+import {motion} from 'framer-motion'
 const About = () => {
   useEffect(()=>{
     Aos.init({duration: 1000})
@@ -11,7 +11,7 @@ const About = () => {
   return (
     <div className='pt-[50px] flex flex-col justify-center gap-6 lg:h-screen'>
       <div className='flex flex-col md:flex-row gap-4 items-center w-[80%] mx-auto p-5 rounded-md shadow-md shadow-purple-500 bg-purple-300' data-aos="zoom-in-left">
-        <img src={mission} alt=""  className='h-72 w-72'/>
+        <motion.img src={mission} alt=""  className='h-72 w-72 cursor-pointer' whileHover={{rotateY : 360}} transition={{type: "tween" , duration:2}}/>
         <div className='flex flex-col gap-2 text-center'>
         <h1 className='text-3xl font-semibold'>Mission</h1>
         <p className='text-lg'>Empowering collaboration and innovation, our mission is to seamlessly connect content creators with businesses, fostering meaningful partnerships in the digital landscape. By providing a dynamic platform that facilitates efficient communication, secure transactions, and insightful analytics, we aim to be the catalyst for creativity, growth, and success in the online content creation ecosystem. Our commitment lies in building a vibrant community where creators thrive, businesses prosper, and authentic collaborations flourish.</p>
@@ -23,7 +23,7 @@ const About = () => {
       
       <div className='flex flex-col md:flex-row gap-4 items-center w-[80%] mx-auto p-5 rounded-md shadow-md shadow-purple-500 bg-purple-300' data-aos="zoom-in-right">
       
-        <img src={vision} alt=""  className='h-72 w-72'/>
+        <motion.img src={vision} alt=""  className='h-72 w-72 cursor-pointer' whileHover={{rotateY : 360}} transition={{type: "tween" , duration:2}}/>
         <div className='flex flex-col gap-2 text-center'>
         <h1 className='text-3xl font-semibold'>Vision</h1>
         <p className='text-lg'>Envisioning a future where the boundaries between content creators and businesses dissolve, our app aspires to be the unparalleled hub for dynamic collaboration in the digital realm. We see a landscape where creativity knows no limits, and partnerships transcend traditional boundaries, resulting in a global network of thriving content ecosystems. By pioneering innovation, fostering inclusivity, and driving transformative connections, our vision is to shape the future of online collaboration, where every creator's voice is amplified, and every business finds its ideal collaborator effortlessly.</p>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,23 @@
-
+import contact from '../assets/contactImg.jpg'
 
 const Contact = () => {
   return (
-    <div className="flex flex-col justify-center items-center min-h-[60vh] gap-7 px-3">
-       <h2 className="text-3xl">We have got you!!</h2>
-       <form action="" className="flex flex-col gap-4 shadow-sm w-full shadow-purple-500 p-4 rounded-md md:w-[40%] xl:w-[25%] mx-auto">
-        <input type="text" placeholder="Your Name" className=" px-3 py-2 rounded-md focus:outline-none"/>
-        <input type="email" placeholder="Your Email" className="px-3 py-2 rounded-md focus:outline-none"/>
-        <textarea className="rounded-md px-3 py-1 focus:outline-none" placeholder="Query"></textarea>
+    <div className=" min-h-[60vh] px-3  py-4 space-y-4">
+       <h2 className="text-3xl text-center">Connect with us!!</h2>
+      <div className='flex flex-col justify-center gap-6 items-center pt-5 min-h-[50vh] w-full md:flex-row'>
+
+       <form action="" className="flex flex-col gap-4 border-2 w-full border-purple-500 p-4 rounded-md md:w-[30%]">
+        <h1 className='text-2xl text-center'>Enter your details</h1>
+        <input type="text" placeholder="Your Name" className=" px-3 py-3 rounded-md focus:outline-none bg-slate-300"/>
+        <input type="email" placeholder="Your Email" className="px-3 py-3 rounded-md focus:outline-none bg-slate-300"/>
+        <textarea className="rounded-md px-3 py-2 focus:outline-none bg-slate-300" placeholder="Query"></textarea>
        </form>
+
+       <div className='overflow-hidden rounded-md shadow-sm shadow-slate-400' id='imgdiv'>
+        <img src={contact} className='md:h-[300px] md:w-[500px]'/>
+       </div>
+      </div>
+      
     </div>
   )
 }
